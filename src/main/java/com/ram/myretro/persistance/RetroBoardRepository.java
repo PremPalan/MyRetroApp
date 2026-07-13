4ddf532b62202af63d298c743224c64c3175a175
Ram Palan <ramrampalan@gmail.com>
2026-07-13 22:56:21 +0530

    chapter5-JPA-rest

--- src/main/java/com/ram/myretro/persistance/RetroBoardRepository.java
+++ src/main/java/com/ram/myretro/persistance/RetroBoardRepository.java
@@ -2,34 +2,11 @@ package com.ram.myretro.persistance;
 
 import com.ram.myretro.board.RetroBoard;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-@Repository
+@RepositoryRestResource(path = "retros",itemResourceRel = "retros", collectionResourceRel = "retros")
 public interface RetroBoardRepository extends JpaRepository<RetroBoard,UUID> {
 }
-
-// public interface JpaRepository<T, ID> extends ListCrudRepository<T, ID>,
-// ListPagingAndSortingRepository<T, ID>, QueryByExampleExecutor<T> {
-//
-//      void flush();
-//
-//      <S extends T> S saveAndFlush(S entity);
-//
-//      <S extends T> List<S> saveAllAndFlush(Iterable<S> entities);
-//
-//      void deleteAllInBatch(Iterable<T> entities);
-//
-//      void deleteAllByIdInBatch(Iterable<ID> ids);
-//
-//      void deleteAllInBatch();
-//
-//      T getReferenceById(ID id);
-//
-//      @Override
-//      <S extends T> List<S> findAll(Example<S> example);
-//
-//      @Override
-//      <S extends T> List<S> findAll(Example<S> example, Sort sort);
-//}
